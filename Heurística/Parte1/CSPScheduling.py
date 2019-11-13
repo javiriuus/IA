60217e6d7a0a8582c81dfafceac903a4668539a3
PaquitoElChocolatero <alexdelacruzalv@gmail.com>
2019-11-13 11:45:45 +0100

    Formalizacion

--- Heurística/Parte1/CSPScheduling.py
+++ Heurística/Parte1/CSPScheduling.py
@@ -1,40 +1,17 @@
-#Import library
+#Importamos la librería
 from constraint import *
 
-#Generate problem
+#Generamos el problema
 problem = Problem()
 
-#Declare variables
+#Clases
+problem.addVariable("clase", ["cienciasDeLaNaturaleza", "cienciasSociales", "lenguaCastellanaYLiteratura", "matematicas", "ingles", "educacionFisica"])
 
-    #Ciencias de la Naturaleza -> 1
-    #Ciencias Sociales -> 2
-    #Lengua castellana y literatura -> 3
-    #Matematicas -> 4
-    #Ingles -> 5
-    #Educacion Fisica -> 6
-    #Andrea -> 11
-    #Juan -> 12
-    #Lucia -> 13
+#Horarios
+problem.addVariable("horario", ["L1", "L2", "L3", "M1", "M2", "M3", "X1", "X2", "X3", "J1", "J2"]])
 
-#Lectures with its possible teachers
-problem.addVariable("cienciasDeLaNaturaleza", range(11, 14))
-problem.addVariable("cienciasSociales", range(11, 14))
-problem.addVariable("lenguaCastellanaYLiteratura", range(11, 14))
-problem.addVariable("matematicas", range(11, 14))
-problem.addVariable("ingles", range(11, 14))
-problem.addVariable("educacionFisica", range(11, 14))
-#Schedules with its possible lectures
-problem.addVariable("l1", range(1, 7))
-problem.addVariable("l2", range(1, 7))
-problem.addVariable("l3", range(1, 7))
-problem.addVariable("m1", range(1, 7))
-problem.addVariable("m2", range(1, 7))
-problem.addVariable("m3", range(1, 7))
-problem.addVariable("x1", range(1, 7))
-problem.addVariable("x2", range(1, 7))
-problem.addVariable("x3", range(1, 7))
-problem.addVariable("j1", range(1, 7))
-problem.addVariable("j2", range(1, 7))
+#Profesores
+problem.addVariable("profesor", ["andrea", "juan", "lucia"])
 
 #Solution
 problem.getSolutions()
