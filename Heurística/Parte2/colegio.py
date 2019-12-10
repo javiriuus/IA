ce9d9fd8699d5af3c7be6564b0bc501f39e07199
PaquitoElChocolatero <alexdelacruzalv@gmail.com>
2019-12-10 09:53:22 +0100

    New State

--- Heurística/Parte2/colegio.py
+++ Heurística/Parte2/colegio.py
@@ -3,33 +3,19 @@ import sys
 class Bus():
     def __init__(self, parada, nMax, alumnos):
         self.parada=parada
-        print(self.parada)
         self.alumnosMax=nMax
-        print(self.alumnosMax)
         self.alumnosBus=[]
-        print(self.alumnosBus)
         self.alumnosRestantes=alumnos
-        print(self.alumnosRestantes)
-        print()
 
-class Alumno():
-    def __init__(self, parada, colegio):
-        self.parada=parada
-        self.colegio=colegio
-
+    def mover(destino):
+        self.parada=destino
+        return 0
 
 class Parada():
-    def __init__(self, nombre, adyacentes, colegio, alumnos):
+    def __init__(self, nombre, adyacentes, colegio):
         self.nombre=nombre
-        print(self.nombre)
-        self.adyacentes=[]
-        print(self.adyacentes)
+        self.adyacentes=adyacentes
         self.colegio=colegio
-        print(self.colegio)
-        self.alumnos=alumnos
-        print(self.alumnos)
-        print()
-
 
 class Simulador():
     def initialize():
@@ -37,9 +23,8 @@ class Simulador():
         flag=0
         flag2=0
 
-        stops={}
+        adjacents={}
         schools={}
-        kids={}
         currentStop=0
 
         fprob=open(sys.argv[1], "r")
@@ -58,7 +43,7 @@ class Simulador():
                                 adyacentes.append(-1)
                             else:
                                 adyacentes.append(int(myLine[i]))
-                        stops[nombre]=adyacentes
+                        adjacents[nombre]=adyacentes
 
                 #Parsea los colegios y sus paradas
                 elif myLine[0][0]=='C' and flag2==0:
@@ -81,7 +66,6 @@ class Simulador():
                     for item in range(len(myLine)):
                         if myLine[item][0]=='P':
                             currentStop=myLine[item]
-                            list=[] #Lista con todos los alumnos de una parada
                         elif myLine[item][0]=='C':
                             Clist=[] #Lista con todos los alumnos de un mismo colegio en una parada
                             #Añadimos colegios a una lista temporal hasta que no queden (por si hubiera más de uno)
@@ -89,20 +73,15 @@ class Simulador():
                                 Clist.append(myLine[item])
                                 myLine[item-1]=str(int(myLine[item-1])-1)
                             for i in Clist:
-                                list.append(i)
-                                kidsList.append(i)
-                            kids[currentStop]=list
+                                kidsList.append((i, currentStop))
                 elif myLine[0][0]=='B':
                     currentStop=myLine[1]
                     nMax=myLine[2]
 
-        #Generamos los elementos del problema
-        print('---BUS---')
         Bus(currentStop, nMax, kidsList)
 
-        print('---PARADAS---')
-        keys=stops.keys()
+        keys=adjacents.keys()
         for stop in keys:
-            Parada(stop, stops[stop], schools[stop] if stop in schools.keys() else 0, kids[stop] if stop in kids.keys() else 0)
+            Parada(stop, adjacents[stop], schools[stop] if stop in schools.keys() else 0)
 
     initialize()
